4a8c08f3a28cc14ad5bc85fbe473e7ed97a16f3a
ErRusso <zucchinaberg69@gmail.com>
2024-06-13 13:48:45 +0200

    test msvc

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ async function main() {
       process.env.CFLAGS = `--target=${targetArch}-${targetPlatform}-gnu`;
     if(targetPlatform === 'windows'){
       args.push('--without-tesseract');
-      process.env.CFLAGS= `--target=${targetArch}-pc-${targetPlatform}-gnu`;
+      process.env.CFLAGS= `--target=${targetArch}-pc-${targetPlatform}-msvc`;
     }
     await runCommand('sh', args, { cwd: ghostpdlFolder });
 
