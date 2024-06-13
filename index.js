d464822a2254a32ff040bf4d343fcc9768d72a53
ErRusso <zucchinaberg69@gmail.com>
2024-06-13 11:07:21 +0200

    fix win64

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ async function main() {
     let args = ['./configure' ];
     if(targetPlatform === 'darwin' && targetArch === 'arm64') 
       process.env.CFLAGS = `--target=${targetArch}-${targetPlatform}-gnu`;
-    if(targetPlatform === 'mingw64'){
+    if(targetPlatform === 'win64'){
       args.push('--without-tesseract');
       process.env.CFLAGS = `--target=${targetArch}-${targetPlatform}-gnu`;
     }
