d160715c05b2f00aa1f653acb80da3a2b726bce7
ErRusso <zucchinaberg69@gmail.com>
2024-06-18 09:41:57 +0200

    first platform

---
+++ npm/@libgs/darwin-aarch64/index.js
@@ -0,0 +1,27 @@
+import { existsSync } from "node:fs";
+async function extractTarGz(tarGzPath, destinationPath) {
+  try {
+    const rGSTarGz = createReadStream(tarGzPath);
+    const wGSTarGz = tar.x({
+      strip: 1,
+      C: destinationPath,
+    });
+    await new Promise((resolve, reject) => {
+      rGSTarGz.pipe(wGSTarGz);
+      wGSTarGz.on("finish", resolve);
+      wGSTarGz.on("error", reject);
+    });
+  } catch (error) {
+    throw new Error(`Failed to extract ${tarGzPath}: ${error.message}`);
+  }
+}
+
+async function main(){
+  const tarArchive = "gs-*-*.tar.gz";
+  if (!existsSync(tarArchive))
+    throw new Error(`File ${tarArchive} does not exist.`);
+  extractTarGz(tarArchive, "./");
+  process.stdout.write(`Extracted ${tarArchive} to ./`);
+}
+
+main().catch(err => {console.error(err); process.exit(1)})
