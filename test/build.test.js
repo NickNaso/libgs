66afed4ee49af97453ae6f6d9b2799c01d68e8d6
ErRusso <zucchinaberg69@gmail.com>
2024-06-25 14:55:36 +0200

    Added proper test (#18)
    * first commit
    
    * changed test name

--- test/build.test.js
+++ test/build.test.js
@@ -1,5 +1,14 @@
 import { expect, test } from "vitest";
+import fs from "node:fs/promises";
 
-test("Test Build", async () => {
-  expect(1).toBe(1);
+const files = await fs.readdir("builds");
+test("Check if all the file exists", async () => {
+  expect(files.length).toBeGreaterThan(0);
+});
+
+test("Ensure all files in the 'builds' directory have a positive size", async () => {
+  for (const file of files) {
+    const stat = await fs.stat(`builds/${file}`);
+    expect(stat.size).toBeGreaterThan(0);
+  }
 });
